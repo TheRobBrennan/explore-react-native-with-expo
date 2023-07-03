715f633426b38089bc3772b56088912773733199
Rob Brennan <rob@therobbrennan.com>
2023-07-03 15:49:19 -0700

    Update Section test to match our new pattern

--- NetworkCheck/components/Section.test.js
+++ NetworkCheck/components/Section.test.js
@@ -1,11 +1,18 @@
-import React from "react";
-import renderer from "react-test-renderer";
-
 import Section from "./Section";
 
+// NOTE: You can use either:
+// renderer.create(<App />) from  "react-test-renderer"
+// or render(<App />) from "@testing-library/react-native"
+import renderer from "react-test-renderer";
+import { render } from "@testing-library/react-native";
+
 describe("<Section />", () => {
-  it("should render", () => {
-    const tree = renderer.create(<Section />).toJSON();
-    expect(tree).toBeDefined();
+  test("should render using react-test-renderer", () => {
+    const result = renderer.create(<Section />).toJSON();
+    expect(result).toBeDefined();
+  });
+  test("should render using render from @testing-library/react-native", () => {
+    const result = render(<Section />);
+    expect(result).toBeDefined();
   });
 });
