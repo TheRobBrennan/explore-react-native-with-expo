b2ed725297bac4f202a3b5413a695442084edf74
Rob Brennan <rob@therobbrennan.com>
2023-07-03 15:37:12 -0700

    Remove commented code

--- NetworkCheck/components/Section.test.js
+++ NetworkCheck/components/Section.test.js
@@ -6,7 +6,6 @@ import Section from "./Section";
 describe("<Section />", () => {
   it("should render", () => {
     const tree = renderer.create(<Section />).toJSON();
-    // expect(tree.children.length).toBe(1);
     expect(tree).toBeDefined();
   });
 });
